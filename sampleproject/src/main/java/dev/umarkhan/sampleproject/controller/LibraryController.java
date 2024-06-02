f2380474f0af5d8fe9ba71f49d45ddafd54a6951
Umar Khan <umar.khan.76801@gmail.com>
2024-06-02 10:52:17 -0400

    changes

--- sampleproject/src/main/java/dev/umarkhan/sampleproject/controller/LibraryController.java
+++ sampleproject/src/main/java/dev/umarkhan/sampleproject/controller/LibraryController.java
@@ -2,7 +2,6 @@ package dev.umarkhan.sampleproject.controller;
 
 import dev.umarkhan.sampleproject.model.Book;
 import dev.umarkhan.sampleproject.repository.LibraryCollectionRepo;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
